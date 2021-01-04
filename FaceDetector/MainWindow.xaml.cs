8c033b4817c01cbb684dd03eb77bc735492496fd
Gareth Parris <gareth@prime23.co.uk>
2021-01-04 16:07:38 +0000

    Updated to use OpenCvSharp4.WpfExtensions .ToBitmapSource() function

--- FaceDetector/MainWindow.xaml.cs
+++ FaceDetector/MainWindow.xaml.cs
@@ -1,19 +1,15 @@
 ﻿// <copyright file="MainWindow.xaml.cs" company="Prime 23 Consultancy Limited">
 // Copyright © 2016-2021 Prime 23 Consultancy Limited. All rights reserved.</copyright>
 
-using System;
 using System.ComponentModel;
 using System.Drawing;
-using System.Drawing.Imaging;
 using System.Threading;
 using System.Windows;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 
 using OpenCvSharp;
 using OpenCvSharp.Extensions;
+using OpenCvSharp.WpfExtensions;
 
-using PixelFormat = System.Drawing.Imaging.PixelFormat;
 using Rect = OpenCvSharp.Rect;
 using Size = OpenCvSharp.Size;
 
@@ -38,41 +34,6 @@ namespace Prime23.FaceDetector
             };
             this.backgroundWorker.DoWork += this.BackgroundWorker_DoWork;
             this.backgroundWorker.ProgressChanged += this.BackgroundWorker_ProgressChanged;
-        }
-
-        private static BitmapSource CreateBitmapSourceFromGdiBitmap(Bitmap bitmap)
-        {
-            if (bitmap == null)
-            {
-                throw new ArgumentNullException(nameof(bitmap));
-            }
-
-            Rectangle rect = new(0, 0, bitmap.Width, bitmap.Height);
-
-            BitmapData bitmapData = bitmap.LockBits(
-                rect,
-                ImageLockMode.ReadWrite,
-                PixelFormat.Format32bppArgb);
-
-            try
-            {
-                int size = (rect.Width * rect.Height) * 4;
-
-                return BitmapSource.Create(
-                    bitmap.Width,
-                    bitmap.Height,
-                    bitmap.HorizontalResolution,
-                    bitmap.VerticalResolution,
-                    PixelFormats.Bgra32,
-                    null,
-                    bitmapData.Scan0,
-                    size,
-                    bitmapData.Stride);
-            }
-            finally
-            {
-                bitmap.UnlockBits(bitmapData);
-            }
         }
 
         private void BackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
@@ -108,7 +69,7 @@ namespace Prime23.FaceDetector
         private void BackgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
         {
             Bitmap frameBitmap = (Bitmap)e.UserState;
-            this.Image.Source = CreateBitmapSourceFromGdiBitmap(frameBitmap);
+            this.Image.Source = frameBitmap.ToBitmapSource();
         }
 
         private void MainWindow_OnClosing(object sender, CancelEventArgs e)
