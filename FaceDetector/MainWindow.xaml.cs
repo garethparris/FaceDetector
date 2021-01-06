764f7cf0cb40293b7c20f66db16316ab9a3756d4
Gareth Parris <gareth@prime23.co.uk>
2021-01-06 15:01:16 +0000

    Added eye detection logic

--- FaceDetector/MainWindow.xaml.cs
+++ FaceDetector/MainWindow.xaml.cs
@@ -1,8 +1,11 @@
 ﻿// <copyright file="MainWindow.xaml.cs" company="Prime 23 Consultancy Limited">
 // Copyright © 2016-2021 Prime 23 Consultancy Limited. All rights reserved.</copyright>
 
+using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
+using System.Linq;
 using System.Threading;
 using System.Windows;
 
@@ -10,6 +13,7 @@ using OpenCvSharp;
 using OpenCvSharp.Extensions;
 using OpenCvSharp.WpfExtensions;
 
+using Point = OpenCvSharp.Point;
 using Rect = OpenCvSharp.Rect;
 using Size = OpenCvSharp.Size;
 
@@ -17,16 +21,21 @@ namespace Prime23.FaceDetector
 {
     public partial class MainWindow
     {
+        private static readonly Scalar EyeColor = new(255, 0, 0);
+        private static readonly Scalar FaceColor = new(255, 0, 255);
+
         private readonly BackgroundWorker backgroundWorker;
         private readonly VideoCapture capture;
-        private readonly CascadeClassifier cascadeClassifier;
+        private readonly CascadeClassifier eyesCascadeClassifier;
+        private readonly CascadeClassifier faceCascadeClassifier;
 
         public MainWindow()
         {
             this.InitializeComponent();
 
             this.capture = new VideoCapture();
-            this.cascadeClassifier = new CascadeClassifier(@"haarcascade_frontalface_default.xml");
+            this.faceCascadeClassifier = new CascadeClassifier(@"haarcascade_frontalface_alt.xml");
+            this.eyesCascadeClassifier = new CascadeClassifier(@"haarcascade_eye_tree_eyeglasses.xml");
 
             this.backgroundWorker = new BackgroundWorker
             {
@@ -44,19 +53,26 @@ namespace Prime23.FaceDetector
             {
                 using (Mat frameMat = this.capture.RetrieveMat())
                 {
-                    Rect[] rects = this.cascadeClassifier.DetectMultiScale(
-                        frameMat,
-                        1.1,
-                        5,
-                        HaarDetectionType.ScaleImage,
-                        new Size(30, 30));
+                    Rect[] faceRects = this.faceCascadeClassifier.DetectMultiScale(frameMat);
 
-                    foreach (Rect rect in rects)
+                    foreach (Rect faceRect in faceRects)
                     {
-                        Cv2.Rectangle(frameMat, rect, Scalar.Red);
+                        Point point = new(faceRect.X + faceRect.Width / 2, faceRect.Y + faceRect.Height / 2);
+                        Cv2.Ellipse(frameMat, point, new Size(faceRect.Width / 2, faceRect.Height / 2), 0, 0, 360, FaceColor);
+
+                        Mat faceRoi = frameMat.SubMat(faceRect);
+
+                        Rect[] eyeRects = this.eyesCascadeClassifier.DetectMultiScale(faceRoi);
+
+                        foreach (Rect eyeRect in eyeRects)
+                        {
+                            Point eyeCenter = new(faceRect.X + eyeRect.X + eyeRect.Width / 2, faceRect.Y + eyeRect.Y + eyeRect.Height / 2);
+                            int radius = (int)Math.Round((eyeRect.Width + eyeRect.Height) * .25);
+                            Cv2.Circle(frameMat, eyeCenter, radius, EyeColor, 4);
+                        }
                     }
 
-                    this.Dispatcher.Invoke(() => this.Info.Text = $"{rects.Length} faces detected");
+                    this.Dispatcher.Invoke(() => this.Info.Text = $"{faceRects.Length} faces detected");
 
                     Bitmap frameBitmap = frameMat.ToBitmap();
                     bgWorker.ReportProgress(0, frameBitmap);
@@ -76,12 +92,14 @@ namespace Prime23.FaceDetector
         {
             this.backgroundWorker.CancelAsync();
             this.capture.Dispose();
-            this.cascadeClassifier.Dispose();
+            this.faceCascadeClassifier.Dispose();
+            this.eyesCascadeClassifier.Dispose();
         }
 
         private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
         {
             this.capture.Open(0, VideoCaptureAPIs.ANY);
+
             if (!this.capture.IsOpened())
             {
                 this.Close();
